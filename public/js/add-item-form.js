3dc1969e8f2f6bf1710e0f4130deed028a774f42
Frederick Ancheta <fancheta@fancheta-laptop.(none)>
2013-01-30 20:57:08 -0800

    Get some of the layout going here. Prettify.

--- public/js/add-item-form.js
+++ public/js/add-item-form.js
@@ -1,11 +1,11 @@
 $(function(){
 
   $("body").on("click", ".item-cancel-button", function(){
-    $(this).parent().dialog("close");
+    $(this).closest(".add-item-form").dialog("close");
   });
 
   $("body").on("click", ".item-create-button", function(){
-    var $f = $(this).parent();
+    var $f = $(this).closest(".add-item-form");
 
     var queueId = $f.attr("value");
 
@@ -25,4 +25,15 @@ $(function(){
     $f.dialog("close");
   });
 
+});
+
+
+$(function(){
+  var limit = 80;
+  $(".add-item-form textarea").keyup(function(){
+    var curVal = $(this).val();
+    if (curVal.length >= limit) {
+      $(this).val(curVal.substr(0, limit));
+    }
+  });
 });
