568fb51c82b0daba2b1e39398fd7f923fad86cb9
Frederick Ancheta <fancheta@fancheta-laptop.(none)>
2013-02-03 13:22:28 -0800

    what

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
-require 'haml'
 
 get "/" do
   @javascripts = ["jquery-1.9.0.min.js", "jquery-ui-1.10.0.custom.min.js", "data.js", "item-data.js", "temple.js", "application.js", "add-item-form.js", "item.js", "item-poll.js"]
